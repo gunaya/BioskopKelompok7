b621024b68197c01057cdc5c101cddf61d48ab54
Gunaya <gunayahokage@gmail.com>
2018-05-24 19:37:03 +0800

    EVERYTHING FIX, Maybe

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,12 +52,6 @@
           <ul class="navbar-nav text-uppercase ml-auto">
             @guest
                 <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#services">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-                </li>
-                <li class="nav-item">
                     <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
                 </li>
                 <li class="nav-item dropdown">
